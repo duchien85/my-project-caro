818ff19ab22095f7a38d778fd64b5227dab682cb
trungdovan87@gmail.com <trungdovan87@gmail.com@f194247f-c2b4-16be-79c0-e4fc8a653888>
2012-01-10 09:24:34 +0000

    animation

--- trunk/DifferentResolution/src/com/gsn/caro/BoardStage.java
+++ trunk/DifferentResolution/src/com/gsn/caro/BoardStage.java
@@ -1,13 +1,14 @@
 package com.gsn.caro;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.gsn.caro.asset.ImageAsset;
 import com.gsn.engine.GsnBoardStage;
-import com.gsn.engine.layout.GsnRectangle;
+import com.gsn.engine.layout.GsnAnimation;
 import com.gsn.engine.layout.GsnRepeatingSprite;
-import com.gsn.engine.layout.GsnSprite;
 import com.gsn.engine.layout.GsnTableLayout;
 
 public class BoardStage extends GsnBoardStage {
@@ -16,15 +17,14 @@ public class BoardStage extends GsnBoardStage {
 	ClockSprite meTimer;
 	ClockSprite otherTimer;	
 	GsnTableLayout menuLayout;
+	GsnAnimation animation;
 	
 	Image board;
 	int tmp = 50;
 	public BoardStage(float width, float height, boolean stretch) {
 		super(width, height, stretch);
 		menuLayout = createMenuLayout(true, 0.2f);
-		menuBG = new GsnRepeatingSprite(asset.menuBG, menuLayout.x, menuLayout.y, menuLayout.width, menuLayout.height);		
-		//menuLayout.getBoundingRectangle().scaleAndPutSprite(menuBG);
-		//System.out.println("menuBG : " + GsnRectangle.toString(menuBG));
+		menuBG = new GsnRepeatingSprite(asset.menuBG, menuLayout.x, menuLayout.y, menuLayout.width, menuLayout.height);
 		
 		menuLayout.newRow(1f);
 		menuLayout.addList(0.4f, 0.2f, 0.4f);		
@@ -32,13 +32,23 @@ public class BoardStage extends GsnBoardStage {
 		meTimer = new ClockSprite(menuLayout.list.get(0));
 		meTimer.setTime(6);
 		otherTimer = new ClockSprite(menuLayout.list.get(2));
+		
+		animation = new GsnAnimation(0.2f, asset.winAni, false, 100, 100);
+		animation.start();
 	}
+	
+	float time; 
+	
 	@Override
 	public void localDraw(SpriteBatch batcher) {
 		// TODO Auto-generated method stub
+		time += Gdx.graphics.getDeltaTime();
 		menuBG.draw(batcher);	
 		meTimer.draw(batcher);
 		otherTimer.draw(batcher);
+		
+		animation.act(Gdx.graphics.getDeltaTime());
+		animation.draw(batcher);
 	}
 	@Override
 	public boolean localTouchUp(float x, float y, int pointer, int button) {
@@ -49,6 +59,18 @@ public class BoardStage extends GsnBoardStage {
 	public boolean globalTouchUp(float x, float y, int pointer, int button) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		switch (keycode){
+		case Input.Keys.F1:
+			animation.start();
+			break;
+		}
+			
+		return super.keyDown(keycode);
 	}
 
 }
